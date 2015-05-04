0fbf5ffc830bcfa8c901dfe48c72f366d1ac3115
Quentin Gautier <qkgautier@eng.ucsd.edu>
2015-05-03 17:59:25 -0700

    Initial commit

---
+++ barrier.h
@@ -0,0 +1,55 @@
+#ifndef _BARRIER_H
+#define _BARRIER_H
+
+#include <mutex>
+#include <cassert>
+#include <condition_variable>
+#include <vector>
+#include <atomic>
+
+using namespace std;
+
+#ifdef LOG_BARRIER
+class barrier
+{
+public:
+    // Fill in your definition of the barrier constructor
+    barrier(int NT = 2) { }
+    // Fill in your definition of the barrier synchronization function
+    void bsync(int TID) {
+    }
+};
+#else
+class barrier
+{
+public:
+  explicit barrier(int a_numThreads):m_nt(a_numThreads), m_ndone(0){ };
+  inline void bsync(int dummy);
+  
+private:
+  barrier(const barrier&);  // disable copying barriers.
+  barrier(); //no null constructor.
+  barrier& operator=(const barrier&); // disable assignment
+  int m_nt;
+  int m_ndone;
+  std::mutex m_mutex;
+  std::condition_variable m_cv;
+};
+
+inline void barrier::bsync(int dummy)
+{
+  std::unique_lock<std::mutex> lk(m_mutex,std::defer_lock);
+  lk.lock();
+  if(++m_ndone < m_nt)
+     m_cv.wait(lk);
+  else {
+      m_ndone = 0;
+      m_cv.notify_all();
+      return;
+    }
+}
+
+
+#endif
+#endif
+
---
+++ elim.cpp
@@ -0,0 +1,70 @@
+/*
+* Unblocked LU decomposition
+* based on a parallel code written by
+*  Anu Rao 11/1/94
+*
+* Code modifications made by 
+* Scott B. Baden, UCSD, 5/3/15
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "cblock.h"
+using namespace std;
+
+//
+// Globals
+//
+
+extern double **A, **R;
+extern control_block cb;
+
+//
+// External Functions
+//
+double getTime();
+
+
+//
+// Gaussian Elmination
+//
+
+void elim()
+{
+  int i, j, k, Mx;
+
+// If we get stuck, we can count the number of row swaps
+// Do this for a small matrix
+// int swaps = 0;
+
+  for ( k = 0; k < cb.N; k++ ) {
+      if (cb.partialPivoting){ /* Partial Pivoting */
+        Mx = k;
+        for ( i = k+1; i < cb.N; i++ ) {
+            if (fabs(A[i][k]) > fabs(A[Mx][k]))
+                Mx = i;
+        }
+        if (Mx > k){
+    //        swaps++;
+            for ( j = k; j < cb.N; j++ ){
+                double t = A[Mx][j];
+                A[Mx][j] = A[k][j];
+                A[k][j] = t;
+            }
+        }
+      } /* End Partial Pivoting */
+
+    for ( i = k+1; i < cb.N; i++ ) 
+      A[i][k] /= A[k][k];  
+
+    for ( i = k+1; i < cb.N; i++ ) {
+      double Aik = A[i][k];
+      double *Ai = A[i];
+      for ( j = k+1; j < cb.N; j++ ) 
+        Ai[j] -= Aik * A[k][j];
+    }  
+  }
+}
