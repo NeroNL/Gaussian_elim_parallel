7f1e566a7fe0f0e0cdfa16f22641e815b0dd8d87
Nero Li <ljp2745559@gmail.com>
2015-05-12 14:48:51 -0700

    testing syntax log barrier

--- barrier.h
+++ barrier.h
@@ -10,14 +10,87 @@
 using namespace std;
 
 #ifdef LOG_BARRIER
+
+struct lock_control{
+  atomic_flag parent_lock = ATOMIC_FLAG_INIT;
+  atomic_flag left_lock = ATOMIC_FLAG_INIT;
+  atomic_flag right_lock = ATOMIC_FLAG_INIT;
+  bool left = false;
+  bool right = false;
+  bool parent = false;
+};
+
 class barrier
 {
+  vector<int> v_a (cb.NT, 0);
+  vector<lock_control> v_lc (cb.NT);
+
 public:
     // Fill in your definition of the barrier constructor
-    barrier(int NT = 2) { }
+    barrier(int NT = 2) {
+      for(unsigned int i = 0; i < cb.NT; i++){
+        (v_lc.at(i)).parent_lock.set_and_test(memory_order_acquire);
+        (v_lc.at(i)).left_lock.set_and_test(memory_order_acquire);
+        (v_lc.at(i)).right_lock.set_and_test(memory_order_acquire);
+      }
+    }
     // Fill in your definition of the barrier synchronization function
     void bsync(int TID) {
+      v_a.at(TID) = TID;
+      int parent = (TID-1)/2;
+      int left_child = TID*2+1;
+      int right_child = TID*2+2;
+      bool left_c = true;
+      bool right_c = true;
+
+      if(parent < 0){
+        parent = 0;
+      }
+      if(left_child > cb.NT-1){
+        (v_lc.at(TID)).left_lock.clear(memory_order_release);
+        (v_lc.at(TID)).left = true;
+      }
+      if(right_child > cb.NT-1){
+        (v_lc.at(TID)).right_lock.clear(memory_order_release);
+        (v_lc.at(TID)).right = true;
+      }
+
+      if((v_lc.at(TID)).left && (v_lc.at(TID)).right){
+        (v_lc.at(TID)).parent_lock.clear(memory_order_release);
+        (v_lc.at(TID)).parent = true;
+        return ;
+      }
+      else{
+        while((v_lc.at(TID)).left != left_c ||
+              (v_lc.at(TID)).right != right_c);
+      }
+
+      /*bool left_c = (v_lc.at(left_child)).parent;
+      bool right_c = (v_lc.at(left_child)).parent;
+
+
+      if(left_c && right_c){
+        v_lc.at(TID)).parent_lock.clear(memory_order_release);
+         (v_lc.at(TID)).parent = true;
+        return;
+      }
+      else
+        (v_lc.at(TID)).parent_lock.set_and_test(memory_order_acquire);
+
+      if(!left_c)
+        (v_lc.at(TID)).left_lock.set_and_test(memory_order_acquire);
+      else
+        (v_lc.at(TID)).left_lock.clear(memory_order_release);
+
+      if(!right_c)
+         (v_lc.at(TID)).right_lock.set_and_test(memory_order_acquire);
+      else
+        (v_lc.at(TID)).right_lock.clear(memory_order_release);
+
+      */
+
     }
+
 };
 #else
 class barrier
