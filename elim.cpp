8cc6d1117e8d87c0a0cba9e13fc2b08f9bfda08c
Nero Li <ljp2745559@gmail.com>
2015-05-14 11:46:51 -0700

    testing data mistake

--- elim.cpp
+++ elim.cpp
@@ -78,7 +78,8 @@ void parallel_elim(int startIndex, int increment, int k0){
     while(k < cb.N){
       for ( int i = startIndex+k+1; i < cb.N; i+=increment ) {
         //count.bsync(k);
-        //cout << "thread number is: " << this_thread::get_id() << " i is : " << i << " startindex is: " << startIndex << " k is: " << k << endl;
+        //cout << "thread number is: " << this_thread::get_id() << " i is : " << i << 
+        cout << " startindex is: " << startIndex << " k is: " << k << endl;
         A[i][k] /= A[k][k];
       }
 
